7976c9c2539344d79b8a514ce958754bbf5b22b2
Gisler Garces <gislersoft@hotmail.com>
2018-10-03 14:05:20 -0500

    Agregando audios de shita para cuando se llega a la primera mission

---
+++ Assets/Audio/Scripts/AudioOnStartScript.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * @autor Gisler Garcés <gislersoft@hotmail.com>
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOnStartScript : MonoBehaviour {
+
+	public AudioClip clip2;
+	public float delayTimeClip2 = 100;
+    IEnumerator Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length + delayTimeClip2);
+        audio.clip = clip2;
+        audio.Play();
+    }
+}
