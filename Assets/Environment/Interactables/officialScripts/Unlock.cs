7fd6f23d403b67f2aa5d9594271fe6164ff59eae
sebastianq96 <sebastian.quintero_o@uao.edu.co>
2018-10-08 23:41:09 -0500

    Agregando scripts para deshabilitar centinela y habilitar puertas

---
+++ Assets/Environment/Interactables/officialScripts/Unlock.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unlock : MonoBehaviour {
+    public BoxCollider door;
+    public BoxCollider door2;
+    public Shooting shooting;
+    public Light ligh;
+
+
+    bool onswitch=false;
+    bool open = false;
+    bool shoot = true;
+	// Use this for initialization
+	void Start () {
+        door = GameObject.Find("doorTrigger").GetComponent<BoxCollider>();
+        door2 = GameObject.Find("doorTrigger2").GetComponent<BoxCollider>();
+        shooting = GameObject.Find("SentinelHead").GetComponent<Shooting>();
+        ligh = GameObject.Find("PointLightSwitch").GetComponent<Light>();
+        door.enabled = false;
+        door2.enabled = false;
+        shooting.enabled = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (onswitch) {
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                open = true;
+                shoot = false;
+                door.enabled=open;
+                door2.enabled = open;
+                shooting.enabled = shoot;
+                ligh.color = new Color(37, 240, 30, 255);
+                ligh.intensity = 0.01f;
+            }
+            
+}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("P1"))
+        {
+            onswitch = true;
+            Debug.Log("entro");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("P1"))
+        {
+            onswitch = false;
+        }
+    }
+}
