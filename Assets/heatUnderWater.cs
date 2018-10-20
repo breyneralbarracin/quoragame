6842d1b0561a0a66df9519f7c3ef47218f7a561b
cristianreyes214 <cristian.reyes@uao.edu.co>
2018-10-20 14:53:16 -0500

    añadiendo codigo para reducir calor dentro y fuera del agua

---
+++ Assets/heatUnderWater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heatUnderWater : MonoBehaviour {
+    GameObject barraCalor;
+    private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "P1")
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 10f)
+            {
+                barraCalor = GameObject.Find("calor");
+                barraCalor.gameObject.GetComponent<Heat>().HeatDamageWater();
+                timer = 0f;
+            }
+
+        }
+    }
+}
